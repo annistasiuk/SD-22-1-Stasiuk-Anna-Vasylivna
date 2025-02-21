01e84746f885c1eea9d03fd8870bbb7fc5f94ca1
annistasiuk <annistasiuk@gmail>
2025-02-21 12:40:24 +0200

    Added my new line to first_task.py

--- first_task.py
+++ first_task.py
@@ -1,4 +1,4 @@
 print("Hi, my name is Stasiuk Anna Vasylivna and I’m studying software development in college")
 print("Hi, my name is Senchak Karina Ivanivna  and I’m studying software development in college")
 print("Hi, my name is Oleksiuk Ivanna Victorivna  and I’m studying software development in college")
-
+print("Task completed")

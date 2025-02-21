a66e6a8e75464e5cc02bd3b3b0b9675fdfc83ae2
Каріна Сенчак <senchak100gmail.com@MacBook-Air-Karina-2.local>
2025-02-21 12:11:07 +0200

    My line

--- first_task.py
+++ first_task.py
@@ -1,1 +1,2 @@
 print("Hi, my name is Stasiuk Anna Vasylivna and I’m studying software development in college")
+print("Hi, my name is Senchak Karina Ivanivna  and I’m studying software development in college")
